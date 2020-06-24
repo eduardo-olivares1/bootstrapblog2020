ca316e3ab3cdc2d57c3b576b92ea5f95069c60d5
Eduardo Olivares <eduardo.olivares@sjsu.edu>
2020-06-24 00:09:41 -0700

    Added post excerpt and added conditionals in case of no posts

--- front-page.php
+++ front-page.php
@@ -3,27 +3,27 @@
 <div class="container mb-4">
     <h1 class="display-4 text-center"><?php the_title();?></h1>
 
-    <div class="card">
+    <?php get_template_part('includes/section', 'content');?>
+    <hr class="mt-4">
+    <?php
+        $args = array( 'numberposts' => '1');
+        $recent_posts = wp_get_recent_posts( $args );
+        if (empty($recent_posts) != true):
+        foreach( $recent_posts as $recent ):
+        ?>
+
+    <div class="card text-center my-4">
         <div class="card-header">
             Latest Post
         </div>
         <div class="card-body">
 
-        <?php
-        $args = array( 'numberposts' => '1');
-        $recent_posts = wp_get_recent_posts( $args );
-        foreach( $recent_posts as $recent ):
-        ?>
-
-       <h2 class="card-title"><?php the_title($recent["post_title"]);?></h2>
-       <?php echo '<p class="card-text">With supporting text below as a natural lead-in to additional content.</p><a href="'. get_permalink($recent["ID"]) .'" class="btn btn-primary">Read latest post</a>' ?>
-
-       <?php endforeach;?>
-
+       <h2 class="card-title"><?php echo $recent["post_title"];?></h2>
+       <?php echo '<p class="card-text">'.get_the_excerpt($recent["ID"]).'</p><a href="'. get_permalink($recent["ID"]) .'" class="btn btn-primary">Read latest post</a>';?>
         </div>
     </div>
-
-    <?php get_template_part('includes/section', 'content');?>
+    <?php endforeach;?>
+    <?php endif;?>
 
 
 
