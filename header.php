0d59f720a58f517d4f3f16686de77dbf899d9980
Eduardo Olivares <eduardo.olivares@sjsu.edu>
2020-06-23 18:12:58 -0700

    added meta viewport for mobile

--- header.php
+++ header.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?php echo get_bloginfo( 'name' ) ?> | <?php the_title();?></title>
     <?php wp_head();?>
 </head>
