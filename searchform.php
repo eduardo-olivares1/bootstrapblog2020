9e950a468e5a36753403b6ef17b829cd213252c4
Eduardo Olivares <eduardo.olivares@sjsu.edu>
2020-06-23 21:55:22 -0700

    added visuallyhidden class for accessibility

--- searchform.php
+++ searchform.php
@@ -1,4 +1,5 @@
 <form action="/" method="get" class="form-inline my-2 my-lg-0">
+    <label class="visuallyhidden" for="search">Search</label>
     <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" name="s" id="search" value="<?php the_search_query();?>" required>
     <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">Search</button>
 </form>
