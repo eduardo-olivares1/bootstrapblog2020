163172024f5ece5bfd4f05ffa41b5a3c3eb8480c
Eduardo Olivares <eduardo.olivares@sjsu.edu>
2020-06-24 21:11:03 -0700

    capitalized u in regex

--- functions.php
+++ functions.php
@@ -68,7 +68,7 @@ add_filter('the_content', 'remove_image_wrapper');
 
 //Find any image classes in content and add bootstrap responsive image classes
 function force_responsive_img($content){
-    return preg_replace('/<p>\s*(<a .*>)?\s*(<img .* \/>)\s*(<\/a>)?\s*<\/p>/iU', '$1 img-fluid my-3 $2', $content);
+    return preg_replace('/(<img.*class="[\w\s-*]*)(".*>)/iU', '$1 img-fluid my-3 $2', $content);
 }
 
 add_filter('the_content', 'force_responsive_img');
