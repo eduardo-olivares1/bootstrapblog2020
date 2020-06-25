b6b08da6046a48276b5ced0fe559bb6018ceac75
Eduardo Olivares <eduardo.olivares@sjsu.edu>
2020-06-24 19:18:37 -0700

    updated to use official handbook structure

--- includes/section-content.php
+++ includes/section-content.php
@@ -1,5 +1,7 @@
-<?php if(have_posts()): while(have_posts()): the_post();?>
-
-    <?php the_content();?>
-
-<?php endwhile; else: endif;?>
+<?php
+if ( have_posts() ) :
+    while ( have_posts() ) : the_post();
+        the_content();
+    endwhile;
+endif;
+?>
